820ea2059d36abb0b1899d9a8233e3575412eded
Nick Sardo <nicksardo@gmail.com>
2020-05-09 08:51:43 -0700

    Fix imports

--- example/websocket.go
+++ example/websocket.go
@@ -1,61 +1,62 @@
 package main
 
 import (
-    "net/http"
-    "github.com/gorilla/websocket"
-	"github.com/NickSardo/GoTime"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"github.com/nicksardo/gotime"
 )
 
 var upgrader *websocket.Upgrader
 
 func init() {
-    upgrader = &websocket.Upgrader{ReadBufferSize: 1024, WriteBufferSize: 1024}
+	upgrader = &websocket.Upgrader{ReadBufferSize: 1024, WriteBufferSize: 1024}
 }
 
 type connection struct {
-    // The websocket connection.
-    ws *websocket.Conn
+	// The websocket connection.
+	ws *websocket.Conn
 
-    // Buffered channel of outbound messages.
-    send chan []byte
+	// Buffered channel of outbound messages.
+	send chan []byte
 }
 
 func (c *connection) reader() {
-    for {
-        _, message, err := c.ws.ReadMessage()
-        if err != nil {
-            break
-        }
+	for {
+		_, message, err := c.ws.ReadMessage()
+		if err != nil {
+			break
+		}
 
-        switch string(message){
-        case "time":
-            c.ws.WriteMessage(websocket.TextMessage, []byte(gotime.Now()))
-        default:
-            // do nothing
-        }
+		switch string(message) {
+		case "time":
+			c.ws.WriteMessage(websocket.TextMessage, []byte(gotime.Now()))
+		default:
+			// do nothing
+		}
 
-    }
-    c.ws.Close()
+	}
+	c.ws.Close()
 }
 
 func (c *connection) writer() {
-    for message := range c.send {
-        err := c.ws.WriteMessage(websocket.TextMessage, message)
-        if err != nil {
-            break
-        }
-    }
-    c.ws.Close()
+	for message := range c.send {
+		err := c.ws.WriteMessage(websocket.TextMessage, message)
+		if err != nil {
+			break
+		}
+	}
+	c.ws.Close()
 }
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
-    ws, err := upgrader.Upgrade(w, r, nil)
-    if err != nil {
-        return
-    }
-    c := &connection{send: make(chan []byte, 256), ws: ws}
-    //h.register <- c
-    //defer func() { h.unregister <- c }()
-    //go c.writer()
-    go c.reader()
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return
+	}
+	c := &connection{send: make(chan []byte, 256), ws: ws}
+	//h.register <- c
+	//defer func() { h.unregister <- c }()
+	//go c.writer()
+	go c.reader()
 }
